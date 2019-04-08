d44e849bad6569fe782b9ce22f93d46db2c2d1c1
Nicolas <n.soto4897@gmail.com>
2019-04-08 18:56:16 -0500

    added scraping logic and saves to json

--- index.js
+++ index.js
@@ -1,39 +1,40 @@
-const fetch = require('node-fetch');
-const cheerio = require('cheerio');
+
+const express = require('express');
+const scrape = require('./scrape');
 
 
-// const puppeteer = require('puppeteer');
-const url = 'http://www.lewisu.edu/experts/wordpress/index.php/blog/'
 
-function fetchPosts() {
-  return fetch(url).then(response => response.text());
-}
+const app = express();
 
-fetchPosts().then(body => {
-  const posts = [];
-  const $ = cheerio.load(body);
-  $('.type-post').each(function (i, element) {
-    const $element = $(element);
-    const $image = $element.find('div div a img');
-    const $title = $element.find('div.eight a')
-    const $summary = $element.find('div.entry-summary p')
-    const $link = $element.find('div.eight a')
-    const $author = $element.find('span.author a.url')
-    // console.log($link.attr('href'))
-    // console.log($image.attr('src'));
-    // console.log($title.text())
-    // console.log($summary.text());
-    const post = {
-      image: $image.attr('src'),
-      title: $title.text(),
-      summary: $summary.text(),
-      link: $link.attr('href'),
-      author: $author.text()
-    }
-    posts.push(post);
 
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'yeet'
   })
-  console.log(posts)
-
 })
+
+app.get('/blogs', (req, res) => {
+  scrape
+    .fetchPosts()
+    .then(posts => {
+      res.json(posts)
+    })
+})
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Listening on ${port}`)
+})
+
+
+
+
+
+
+
+
+
+
 
---
+++ scrape.js
@@ -0,0 +1,45 @@
+const cheerio = require('cheerio');
+const fetch = require('node-fetch')
+
+
+function fetchPosts() {
+  return fetch('http://www.lewisu.edu/experts/wordpress/index.php/blog/')
+    .then(response => response.text())
+    .then(body => {
+      const posts = [];
+      const $ = cheerio.load(body);
+      $('.type-post').each(function (i, element) {
+        const $element = $(element);
+        const $image = $element.find('div div a img');
+        const $title = $element.find('div.eight a')
+        const $summary = $element.find('div.entry-summary p')
+        const $link = $element.find('div.eight a')
+        const $author = $element.find('span.author a.url')
+        // console.log($link.attr('href'))
+        // console.log($image.attr('src'));
+        // console.log($title.text())
+        // console.log($summary.text());
+
+        const post = {  
+
+          image: $image.attr('src'),
+          title: $title.text(),
+          summary: $summary.text(),
+          link: $link.attr('href'),
+          author: $author.text()
+        }
+
+      posts.push(post);
+    });
+      console.log(posts)
+      const blog = {
+        blogs: posts
+      }
+      console.log(blog)
+    return blog
+  });
+}
+
+module.exports = {
+  fetchPosts
+};
