eac848e7c2996bc9bf9a27f8674e403bbb3ffdfe
Nicolas Soto <n.soto4897@gmail.com>
2019-04-16 18:57:34 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,15 @@
 
 const express = require('express');
 const scrape = require('./scrape');
-
-
-
+const db = require('./db');
 const app = express();
 
 
-
 app.get('/', (req, res) => {
-  res.json({
-    message: 'yeet'
-  })
+  scrape.fetchPosts().then(posts => {
+    res.json(posts)    
+  })  
 })
-
-app.get('/blog', (req, res) => {
-  scrape
-    .fetchPosts()
-    .then(posts => {
-      res.json(posts)
-    })
-})
-
-
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`Listening on ${port}`)
